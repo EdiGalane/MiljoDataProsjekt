139fc416ba9fdbf581cfb9af6c7a0e77c58a2512
Edi Galane <edigalane@dhcp-10-220-230-122.wlan.ntnu.no>
2025-05-22 18:35:36 +0200

    opprettet klassen DataVisualisering i filen data_vizualisation

---
+++ src/data_vizualisation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plot_tidsserie
+import seaborn as sns 
+
+class DataVisualisering:
+    """
+    Klasse for visualiswering av miljødata. inneholder metoder for å plotte trender,
+    sammenhenger, fordelingen og korrelasjoner basert på analyse av en DataFrame
+    """
+    
